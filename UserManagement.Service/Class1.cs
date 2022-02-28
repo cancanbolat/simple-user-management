6a7f679b16b882cedf11123a7407aec56282f461
Can <can@bfydigital.com>
2022-02-28 21:26:13 +0300

    Add project files.

---
+++ UserManagement.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UserManagement.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/ServiceTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ServiceTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
