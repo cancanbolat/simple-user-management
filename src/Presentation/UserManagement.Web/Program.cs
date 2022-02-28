b850bbb6421cd2650367ed73f6bd7d68d01be182
Can <can@bfydigital.com>
2022-02-28 22:30:11 +0300

    Update Program.cs

--- src/Presentation/UserManagement.Web/Program.cs
+++ src/Presentation/UserManagement.Web/Program.cs
@@ -1,16 +1,27 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace UserManagement.Web
 {
     public class Program
     {
+        private static IConfiguration Configuration
+        {
+            get
+            {
+                string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+                return new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .AddJsonFile($"appsettings.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -20,6 +31,7 @@ namespace UserManagement.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseConfiguration(Configuration);
                     webBuilder.UseStartup<Startup>();
                 });
     }
